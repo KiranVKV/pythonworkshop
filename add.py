3644511e8f4e4025a917930582cf402a8e7423fc
KiranVKV <vattemkirann@gmail.com>
2021-01-29 15:30:36 +0530

    first commit

---
+++ add.py
@@ -0,0 +1,3 @@
+x = int(input("enter:"))
+y = int(input("enter:"))
+print("Addition value is:",x+y)
---
+++ arithimetic operations.py
@@ -0,0 +1,9 @@
+x = int(input("enter:"))
+y = int(input("enter:"))
+print("Addition value is:",x+y)
+print("Subtrsction value is:",x-y)
+print("Multiplicatio value is:",x*y)
+print("divison value is:", x/y)
+print("exponent value is:", x**y)
+print("Floor division value is:", x//y)
+
